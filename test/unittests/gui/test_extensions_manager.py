9c2b0fb0ddd070c9bb896f7b54798ad858c50e3c
Aditya Mehra <aix.m@outlook.com>
2022-02-25 20:15:32 +1030

    update unit test for config

--- test/unittests/gui/test_extensions_manager.py
+++ test/unittests/gui/test_extensions_manager.py
@@ -1,15 +1,31 @@
 from unittest import TestCase, mock
 from mycroft.gui.extensions import ExtensionsManager
 from ..mocks import MessageBusMock
+from mycroft.configuration import Configuration
+from test.util import base_config
 
 PATCH_MODULE = "mycroft.gui.extensions"
 
 # Add Unit Tests For ExtensionManager
 
 class TestExtensionManager:
+    @patch.object(Configuration, 'get')
 
     def test_extension_manager_activate(self):
+        config = base_config()
+        config.merge(
+            {
+                'enclosure': {
+                    'extension': 'Generic',
+                    'generic': {
+                        'homescreen_supported': False
+                    }
+                }
+            })
+        mock_get.return_value = config
+        config['enclosure']['extension'] = 'Generic'
+        config['enclosure']['extension']['generic']['homescreen_supported'] = False
         extension_manager = ExtensionsManager("ExtensionManager", MessageBusMock(), MessageBusMock())
         extension_manager.activate_extension = mock.Mock()
-        extension_manager.activate_extension("SmartSpeaker")
-        extension_manager.activate_extension.assert_any_call("SmartSpeaker")
+        extension_manager.activate_extension("Generic")
+        extension_manager.activate_extension.assert_any_call("Generic")
