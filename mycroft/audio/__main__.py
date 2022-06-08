434ffbdeee1c6f3ff2b9144d692ae18caa848a8e
Daniel McKnight <daniel@neon.ai>
2022-06-08 16:00:31 -0700

    Replace `speech` module for backwards-compat

--- mycroft/audio/__main__.py
+++ mycroft/audio/__main__.py
@@ -18,9 +18,12 @@ from mycroft.lock import Lock as PIDLock  # Create/Support PID locking file
 from mycroft.util import reset_sigint_handler, wait_for_exit_signal, \
     check_for_signal
 
+service = None  # Added for backwards-compat.
+
 
 def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping,
          watchdog=lambda: None):
+    global service
     """Start the Audio Service and connect to the Message Bus"""
     reset_sigint_handler()
     check_for_signal("isSpeaking")
