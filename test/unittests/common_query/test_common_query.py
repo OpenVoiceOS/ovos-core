bc16fda5eb941d3da6c16949bcf9882044548cf7
jarbasai <jarbasai@mailfence.com>
2022-08-19 19:02:09 +0100

    native common query tests

--- test/unittests/common_query/test_common_query.py
+++ test/unittests/common_query/test_common_query.py
@@ -1,9 +1,7 @@
 import json
 import unittest
 
-from ovos_skill_common_query import QuestionsAnswersSkill
-
-from mycroft.skills import FallbackSkill
+from mycroft.skills.intent_services.commonqa_service import CommonQAService
 from ovos_tskill_fakewiki import FakeWikiSkill
 from ovos_utils.messagebus import FakeBus, Message
 
@@ -16,74 +14,34 @@ class TestCommonQuery(unittest.TestCase):
         def get_msg(msg):
             self.bus.emitted_msgs.append(json.loads(msg))
 
-        self.bus.on("message", get_msg)
-
         self.skill = FakeWikiSkill()
         self.skill._startup(self.bus, "wiki.test")
 
-        self.bus.emitted_msgs = []
+        self.cc = CommonQAService(self.bus)
 
-        self.cc = QuestionsAnswersSkill()
-        self.cc._startup(self.bus, "common_query.test")
-
-    def test_skill_id(self):
-        self.assertEqual(self.cc.skill_id, "common_query.test")
-
-        # if running in ovos-core every message will have the skill_id in context
-        for msg in self.bus.emitted_msgs:
-            self.assertEqual(msg["context"]["skill_id"], "common_query.test")
-
-    def test_intent_register(self):
-        # helper .voc files only, no intents
-        self.assertTrue(isinstance(self.cc, FallbackSkill))
-
-        adapt_ents = ["common_query_testQuestion"]
-        for msg in self.bus.emitted_msgs:
-            if msg["type"] == "register_vocab":
-                self.assertTrue(msg["data"]["entity_type"] in adapt_ents)
-
-    def test_registered_events(self):
-        registered_events = [e[0] for e in self.cc.events]
-
-        # common query event handlers
-        common_query = ['question:query.response']
-        for event in common_query:
-            self.assertTrue(event in registered_events)
-
-        # base skill class events shared with mycroft-core
-        default_skill = ["mycroft.skill.enable_intent",
-                         "mycroft.skill.disable_intent",
-                         "mycroft.skill.set_cross_context",
-                         "mycroft.skill.remove_cross_context",
-                         "intent.service.skills.deactivated",
-                         "intent.service.skills.activated",
-                         "mycroft.skills.settings.changed"]
-        for event in default_skill:
-            self.assertTrue(event in registered_events)
-
-        # base skill class events exclusive to ovos-core
-        default_ovos = ["skill.converse.ping",
-                        "skill.converse.request",
-                        f"{self.cc.skill_id}.activate",
-                        f"{self.cc.skill_id}.deactivate"]
-        for event in default_ovos:
-            self.assertTrue(event in registered_events)
+        self.bus.on("message", get_msg)
 
     def test_common_query_events(self):
         self.bus.emitted_msgs = []
-        self.cc.handle_question(Message("fallback_cycle_test",
-                                        {"utterance": "what is the speed of light"}))
+        self.assertEqual(self.cc.skill_id, "common_query.openvoiceos")
+
+        self.bus.emit(Message("common_query.question",
+                              {"utterance": "what is the speed of light"}))
 
         expected = [
+            # original query
+            {'context': {},
+             'data': {'utterance': 'what is the speed of light'},
+             'type': 'common_query.question'},
             # thinking animation
             {'type': 'enclosure.mouth.think',
              'data': {},
              'context': {'destination': ['enclosure'],
-                         'skill_id': 'common_query.test'}},
+                         'skill_id': self.cc.skill_id}},
             # send query
             {'type': 'question:query',
              'data': {'phrase': 'what is the speed of light'},
-             'context': {'skill_id': 'common_query.test'}},
+             'context': {'skill_id': self.cc.skill_id}},
             # skill announces its searching
             {'type': 'question:query.response',
              'data': {'phrase': 'what is the speed of light',
@@ -109,27 +67,27 @@ class TestCommonQuery(unittest.TestCase):
             {'type': 'enclosure.mouth.reset',
              'data': {},
              'context': {'destination': ['enclosure'],
-                         'skill_id': 'common_query.test'}
+                         'skill_id': self.cc.skill_id}
              },
             # tell enclosure about active skill (speak method)
             {'type': 'enclosure.active_skill',
-             'data': {'skill_id': 'common_query.test'},
+             'data': {'skill_id': self.cc.skill_id},
              'context': {'destination': ['enclosure'],
-                         'skill_id': 'common_query.test'}},
+                         'skill_id': self.cc.skill_id}},
             # execution of speak method
             {'type': 'speak',
              'data': {'utterance': 'answer 1',
                       'expect_response': False,
-                      'meta': {'skill': 'common_query.test'},
+                      'meta': {'skill': self.cc.skill_id},
                       'lang': 'en-us'},
-             'context': {'skill_id': 'common_query.test'}},
+             'context': {'skill_id': self.cc.skill_id}},
             # skill callback event
             {'type': 'question:action',
              'data': {'skill_id': 'wiki.test',
                       'phrase': 'what is the speed of light',
                       'callback_data': {'query': 'what is the speed of light',
                                         'answer': 'answer 1'}},
-             'context': {'skill_id': 'common_query.test'}}
+             'context': {'skill_id': self.cc.skill_id}}
         ]
 
         for ctr, msg in enumerate(expected):
