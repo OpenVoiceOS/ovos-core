9c5ae4d7819e7e28d29edc0a08234ca3d71fae54
Daniel McKnight <daniel@neongecko.com>
2023-01-19 16:45:05 -0800

    Update deprecated import

--- mycroft/skills/skill_data.py
+++ mycroft/skills/skill_data.py
@@ -13,15 +13,14 @@
 # limitations under the License.
 #
 """Handling of skill data such as intents and regular expressions."""
-
 # backwards compat imports, do not delete
+from ovos_utils.intents.intent_service_interface import munge_intent_parser
 from ovos_workshop.resource_files import SkillResourceTypes, ResourceType, ResourceFile, \
     QmlFile, DialogFile, VocabularyFile, NamedValueFile, ListFile, TemplateFile, RegexFile, WordFile, \
     CoreResources, UserResources, SkillResources, RegexExtractor, locate_base_directories, \
     locate_lang_directories, find_resource
-
 from mycroft.deprecated.skills import (
     read_value_file, read_translated_file, read_vocab_file,
     load_vocabulary, load_regex, load_regex_from_file, to_alnum,
-    munge_regex, munge_intent_parser
+    munge_regex
 )
