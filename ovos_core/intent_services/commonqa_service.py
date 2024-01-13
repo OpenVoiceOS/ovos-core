951b56c0b2b483826a1c6ae82e1205a356d32dbe
JarbasAi <jarbasai@mailfence.com>
2024-01-13 00:40:50 +0000

    feat/break_ties
    squeeze performance
    
    backwards compat version logs and notes
    
    rm not needed backwards compat
    
            # notify about outdated skill, this can only happen if it is running in standalone mode
            # standalone mode skills can have any version of ovos-workshop >= 0.0.12a49
            # the CommonQuery base class started handling speak also in 0.0.12a49
            # (different PRs -> release automation failure ? )
            # it is impossible to load outdated common_query skills
            # for this method to trigger since ovos-core requires workshop 0.0.15

--- ovos_core/intent_services/commonqa_service.py
+++ ovos_core/intent_services/commonqa_service.py
@@ -8,12 +8,12 @@ from typing import Dict
 from ovos_bus_client.apis.enclosure import EnclosureAPI
 from ovos_bus_client.message import Message
 from ovos_bus_client.session import SessionManager
-from ovos_bus_client.util import get_message_lang
-from ovos_config.config import Configuration
+from ovos_classifiers.opm.heuristics import BM25MultipleChoiceSolver
 from ovos_utils import flatten_list
-from ovos_utils.log import LOG, log_deprecation
+from ovos_utils.log import LOG
 
 import ovos_core.intent_services
+from ovos_config.config import Configuration
 from ovos_workshop.resource_files import CoreResources
 
 
@@ -21,11 +21,12 @@ from ovos_workshop.resource_files import CoreResources
 class Query:
     session_id: str
     query: str
+    lang: str
     replies: list = None
     extensions: list = None
     queried_skills: list = None
-    query_time: float = time.time()
-    timeout_time: float = time.time() + 1
+    query_time: float = 0
+    timeout_time: float = 0
     responses_gathered: Event = Event()
     completed: Event = Event()
     answered: bool = False
@@ -43,8 +44,9 @@ class CommonQAService:
         config = Configuration().get('skills', {}).get("common_query") or dict()
         self._extension_time = config.get('extension_time') or 3
         CommonQAService._EXTENSION_TIME = self._extension_time
-        self._min_response_wait = config.get('min_response_wait') or 2
+        self._min_wait = config.get('min_response_wait') or 2
         self._max_time = config.get('max_response_wait') or 6  # regardless of extensions
+        self.untier = BM25MultipleChoiceSolver()  # TODO - allow plugin from config
         self.bus.on('question:query.response', self.handle_query_response)
         self.bus.on('common_query.question', self.handle_question)
         self.bus.on('ovos.common_query.pong', self.handle_skill_pong)
@@ -56,6 +58,7 @@ class CommonQAService:
             self.common_query_skills = []
         if message.data["skill_id"] not in self.common_query_skills:
             self.common_query_skills.append(message.data["skill_id"])
+            LOG.debug("Detected CommonQuery skill: " + message.data["skill_id"])
 
     def voc_match(self, utterance: str, voc_filename: str, lang: str,
                   exact: bool = False) -> bool:
@@ -128,17 +131,24 @@ class CommonQAService:
         match = None
 
         # exit early if no common query skills are installed
-        if self.common_query_skills is None:
-            # when None means a older ovos-workshop is in use
-            LOG.warning(
-                "you seem to be running an old ovos-workshop version, CommonQuery will wait minimum 2 seconds for skill responses")
-        elif not self.common_query_skills:
-            LOG.info("No Commonquery skills to search")
-            return None
+        if not self.common_query_skills:
+            from ovos_workshop.version import VERSION_BUILD, VERSION_ALPHA
+            # TODO - standalone skills can be any version >=0.0.12a16
+            # common query skills should ensure ovos-workshop >= 0.0.16a7 in requirements.txt
+            # ovos-core currently only requires ovos-workshop 0.0.15
+            if VERSION_BUILD < 16 or (VERSION_BUILD == 16 and 0 < VERSION_ALPHA < 7):
+                LOG.warning("you seem to be running ovos-workshop < 0.0.16a7 , "
+                            f"CommonQuery will wait minimum {self._min_wait} seconds for skills."
+                            f" upgrade ovos-workshop for an extra speedup")
+            else:
+                LOG.info("No CommonQuery skills to search")
+                return None
+        else:
+            LOG.info(f"Gathering answers from skills: {self.common_query_skills}")
 
         for utterance in utterances:
             if self.is_question_like(utterance, lang):
-                message.data["lang"] = lang  # only used for speak
+                message.data["lang"] = lang  # only used for speak method
                 message.data["utterance"] = utterance
                 answered, skill_id = self.handle_question(message)
                 if answered:
@@ -154,15 +164,15 @@ class CommonQAService:
         Send the phrase to CommonQuerySkills and prepare for handling replies.
         """
         utt = message.data.get('utterance')
-        sid = SessionManager.get(message).session_id
-        # TODO: Why are defaults not creating new objects on init?
-        query = Query(session_id=sid, query=utt, replies=[], extensions=[],
-                      query_time=time.time(), timeout_time=time.time() + 1,
+        sess = SessionManager.get(message)
+        query = Query(session_id=sess.session_id, query=utt, lang=sess.lang,
+                      replies=[], extensions=[],
+                      query_time=time.time(), timeout_time=time.time() + self._max_time,
                       responses_gathered=Event(), completed=Event(),
-                      answered=False, queried_skills=self.common_query_skills)
+                      answered=False, queried_skills=[])
         assert query.responses_gathered.is_set() is False
         assert query.completed.is_set() is False
-        self.active_queries[sid] = query
+        self.active_queries[sess.session_id] = query
         self.enclosure.mouth_think()
 
         LOG.info(f'Searching for {utt}')
@@ -174,23 +184,20 @@ class CommonQAService:
         timeout_msg = msg.response(msg.data)
         self.bus.emit(msg)
 
-        query.timeout_time = time.time() + self._max_time
-        timeout = False
-        while not query.responses_gathered.wait(0.5):
+        while not query.responses_gathered.wait(0.1):
             # forcefully timeout if search is still going
             if time.time() > query.timeout_time:
                 if not query.completed.is_set():
                     LOG.debug(f"Session Timeout gathering responses ({query.session_id})")
+                    LOG.warning(f"Timed out getting responses for: {query.query}")
                     timeout = True
                 break
 
-        if timeout:
-            LOG.warning(f"Timed out getting responses for: {query.query}")
         self._query_timeout(timeout_msg)
         if not query.completed.wait(5):
             raise TimeoutError("Timed out processing responses")
         answered = bool(query.answered)
-        self.active_queries.pop(sid)
+        self.active_queries.pop(sess.session_id)
         LOG.debug(f"answered={answered}|"
                   f"remaining active_queries={len(self.active_queries)}")
         return answered, query.selected_skill
@@ -220,24 +227,27 @@ class CommonQAService:
                 LOG.info(f'Answer from {skill_id}')
                 query.replies.append(message.data)
 
+            query.queried_skills.append(skill_id)
+
             # Remove the skill from list of timeout extensions
             if skill_id in query.extensions:
                 LOG.debug(f"Done waiting for {skill_id}")
                 query.extensions.remove(skill_id)
 
-            time_to_wait = (query.query_time + self._min_response_wait -
-                            time.time())
-            if time_to_wait > 0:
-                LOG.debug(f"Waiting {time_to_wait}s before checking extensions")
-                query.responses_gathered.wait(time_to_wait)
-            # not waiting for any more skills
-            if not query.extensions:
-                LOG.debug(f"No more skills to wait for ({query.session_id})")
-                query.responses_gathered.set()
-
             # if all skills answered, stop searching
-            if self.common_query_skills is not None and query.queried_skills == self.common_query_skills:
+            if self.common_query_skills is not None and set(query.queried_skills) == set(self.common_query_skills):
+                LOG.debug("All skills answered")
                 query.responses_gathered.set()
+            else:
+                time_to_wait = (query.timeout_time - time.time())
+                if time_to_wait > 0:
+                    LOG.debug(f"Waiting up to {time_to_wait}s for other skills")
+                    query.responses_gathered.wait(time_to_wait)
+
+                # not waiting for any more skills
+                if not query.extensions and not query.responses_gathered.is_set():
+                    LOG.debug(f"Exiting early, no more skills to wait for session ({query.session_id})")
+                    query.responses_gathered.set()
 
     def _query_timeout(self, message: Message):
         """
@@ -261,17 +271,20 @@ class CommonQAService:
         for response in query.replies:
             if not best or response['conf'] > best['conf']:
                 best = response
-                ties = []
+                ties = [response]
             elif response['conf'] == best['conf']:
                 ties.append(response)
 
         if best:
-            if ties:
-                # TODO: Ask user to pick between ties or do it automagically
-                pass
+            if len(ties) > 1:
+                tied_ids = [m["skill_id"] for m in ties]
+                LOG.info(f"Tied skills: {tied_ids}")
+                answers = {m["answer"]: m for m in ties}
+                best_ans = self.untier.select_answer(query.query,
+                                                     list(answers.keys()),
+                                                     {"lang": query.lang})
+                best = answers[best_ans]
 
-            if not best.get('handles_speech'):
-                self.speak(best['answer'], message.reply("", best))
             LOG.info('Handling with: ' + str(best['skill_id']))
             query.selected_skill = best["skill_id"]
             response_data = {**best, "phrase": search_phrase}
@@ -281,25 +294,3 @@ class CommonQAService:
         else:
             query.answered = False
         query.completed.set()
-
-    def speak(self, utterance: str, message: Message):
-        """Speak a sentence.
-
-        Args:
-            utterance (str): response to be spoken
-            message (Message): Message associated with selected response
-        """
-        log_deprecation("Skills should handle `speak` calls.", "0.1.0")
-        selected_skill = message.data['skill_id']
-        # registers the skill as being active
-        self.enclosure.register(selected_skill)
-
-        lang = get_message_lang(message)
-        data = {'utterance': utterance,
-                'expect_response': False,
-                'meta': {"skill": selected_skill},
-                'lang': lang}
-
-        m = message.forward("speak", data)
-        m.context["skill_id"] = selected_skill
-        self.bus.emit(m)
