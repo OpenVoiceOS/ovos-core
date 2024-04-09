43aebffdde9ccecab06891bfd4666f4bb87033f8
miro <jarbasai@mailfence.com>
2024-04-09 19:47:10 +0100

    keep mycroft imports compat

--- mycroft/skills/intent_services/__init__.py
+++ mycroft/skills/intent_services/__init__.py
@@ -1,14 +1,12 @@
 from ovos_core.intent_services import AdaptService,\
-    ConverseService,\
+    ConverseService, \
     CommonQAService, \
     FallbackService, \
-    PadaciosoService, \
-    PadatiousService
+    PadaciosoService
 from ovos_core.intent_services import IntentMatch
 from mycroft.skills.intent_services.adapt_service import AdaptIntent, IntentBuilder, Intent
 
-try:  # TODO -remove backwards compat import, before 0.0.8, ovos_core module didnt make it into a stable release yet!
-    from ovos_core.intent_services import PadatiousMatcher
+try:
+    from ovos_core.intent_services.padatious_service import PadatiousService, PadatiousMatcher
 except ImportError:
-    from ovos_utils.log import LOG
-    LOG.warning("padatious not installed")
+    from ovos_core.intent_services.padacioso_service import PadaciosoService as PadatiousService
