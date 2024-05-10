083fb4a4709fc399c48903f8edcaa2f641f38194
miro <jarbasai@mailfence.com>
2024-05-10 04:58:15 +0100

    more tests

--- test/end2end/session/test_ocp.py
+++ test/end2end/session/test_ocp.py
@@ -537,3 +537,430 @@ class TestOCPPipeline(TestCase):
             self.assertEqual(m.msg_type, expected_messages[idx])
 
         self.assertEqual(self.core.intent_service.ocp.player_state, PlayerState.STOPPED)
+
+    def test_legacy_next(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.config = {"legacy": True}
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertTrue(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["next"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:next",
+            'mycroft.audio.service.next'  # LEGACY api
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_legacy_prev(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.config = {"legacy": True}
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertTrue(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["previous"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:prev",
+            'mycroft.audio.service.prev'  # LEGACY api
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_pause(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertFalse(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["pause"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:pause",
+            'ovos.common_play.pause'
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_resume(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.PAUSED
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertFalse(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["resume"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:resume",
+            'ovos.common_play.resume'
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_stop(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertFalse(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["stop"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:media_stop",
+            'ovos.common_play.stop',
+            "ovos.common_play.stop.response"
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_next(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertFalse(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["next"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:next",
+            'ovos.common_play.next'
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_prev(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.PLAYING
+        self.core.intent_service.ocp.media_state = MediaState.LOADED_MEDIA
+        self.assertFalse(self.core.intent_service.ocp.use_legacy_audio)
+        messages = []
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+        utt = Message("recognizer_loop:utterance",
+                      {"utterances": ["previous"]},
+                      {"session": sess.serialize(),  # explicit
+                       })
+        self.core.bus.emit(utt)
+
+        # confirm all expected messages are sent
+        expected_messages = [
+            "recognizer_loop:utterance",
+            "ovos.common_play.status",
+            "intent.service.skills.activate",
+            "intent.service.skills.activated",
+            "ovos.common_play.activate",
+            "ocp:prev",
+            'ovos.common_play.previous'
+        ]
+        wait_for_n_messages(len(expected_messages))
+
+        self.assertEqual(len(expected_messages), len(messages))
+
+        for idx, m in enumerate(messages):
+            self.assertEqual(m.msg_type, expected_messages[idx])
+
+    def test_status_matches_not_playing(self):
+        self.assertIsNotNone(self.core.intent_service.ocp)
+        self.core.intent_service.ocp.player_state = PlayerState.STOPPED
+        self.core.intent_service.ocp.media_state = MediaState.NO_MEDIA
+
+        def new_msg(msg):
+            nonlocal messages
+            m = Message.deserialize(msg)
+            if m.msg_type in ["ovos.skills.settings_changed", "gui.status.request"]:
+                return  # skip these, only happen in 1st run
+            messages.append(m)
+            print(len(messages), msg)
+
+        def wait_for_n_messages(n):
+            nonlocal messages
+            t = time.time()
+            while len(messages) < n:
+                sleep(0.1)
+                if time.time() - t > 10:
+                    raise RuntimeError("did not get the number of expected messages under 10 seconds")
+
+        self.core.bus.on("message", new_msg)
+
+        sess = Session("test-session",
+                       pipeline=[
+                           "converse",
+                           "ocp_high"
+                       ])
+
+        # wont match unless PlayerState.Playing
+        for t in ["pause", "resume", "stop", "next", "previous"]:
+            messages = []
+
+            utt = Message("recognizer_loop:utterance",
+                          {"utterances": [t]},
+                          {"session": sess.serialize(),  # explicit
+                           })
+            self.core.bus.emit(utt)
+
+            # confirm all expected messages are sent
+            expected_messages = [
+                "recognizer_loop:utterance",
+                "ovos.common_play.status",
+                "mycroft.audio.play_sound",
+                "complete_intent_failure"
+            ]
+            wait_for_n_messages(len(expected_messages))
+
+            self.assertEqual(len(expected_messages), len(messages))
+
+            for idx, m in enumerate(messages):
+                self.assertEqual(m.msg_type, expected_messages[idx])
