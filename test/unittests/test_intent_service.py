d8359f293ab69194558c5eb273d370ee910c24c8
miro <jarbasai@mailfence.com>
2024-11-02 00:21:36 +0000

    tests moved to adapt repo

--- test/unittests/test_intent_service.py
+++ test/unittests/test_intent_service.py
@@ -12,18 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from copy import deepcopy
 from unittest import TestCase, mock
 
 from ovos_bus_client.message import Message
 from ovos_bus_client.session import IntentContextManager as ContextManager
 from ovos_bus_client.util import get_message_lang
 from ovos_config import Configuration
-from ovos_config.locale import setup_locale
-from ovos_core.intent_services import IntentService
-from ovos_utils.fakebus import FakeBus
-from ovos_workshop.intents import IntentBuilder, Intent as AdaptIntent
-from copy import deepcopy
 from ovos_config import LocalConf, DEFAULT_CONFIG
+from ovos_config.locale import setup_locale
 
 # Setup configurations to use with default language tests
 BASE_CONF = deepcopy(LocalConf(DEFAULT_CONFIG))
@@ -93,11 +90,6 @@ class ContextManagerTest(TestCase):
         self.assertEqual(len(self.context_manager.frame_stack), 0)
 
 
-def check_converse_request(message, skill_id):
-    return (message.msg_type == 'skill.converse.request' and
-            message.data['skill_id'] == skill_id)
-
-
 class TestLanguageExtraction(TestCase):
     @mock.patch.dict(Configuration._Configuration__patch, BASE_CONF)
     def test_no_lang_in_message(self):
@@ -115,144 +107,3 @@ class TestLanguageExtraction(TestCase):
         self.assertEqual(get_message_lang(msg), 'de-DE')
         msg = Message('test msg', data={'lang': 'sv-se'})
         self.assertEqual(get_message_lang(msg), 'sv-SE')
-
-
-def create_old_style_vocab_msg(keyword, value):
-    """Create a message for registering an adapt keyword."""
-    return Message('register_vocab',
-                   {'start': value, 'end': keyword})
-
-
-def create_vocab_msg(keyword, value):
-    """Create a message for registering an adapt keyword."""
-    return Message('register_vocab',
-                   {'entity_value': value, 'entity_type': keyword})
-
-
-class TestIntentServiceApi(TestCase):
-    def setUp(self):
-        self.bus = FakeBus()
-        self.bus.emitted_msgs = []
-
-        def on_msg(m):
-            self.bus.emitted_msgs.append(Message.deserialize(m))
-
-        self.bus.on("message", on_msg)
-
-        self.intent_service = IntentService(self.bus)
-
-    def setup_simple_adapt_intent(self,
-                                  msg=create_vocab_msg('testKeyword', 'test')):
-        self.intent_service.handle_register_vocab(msg)
-
-        intent = IntentBuilder('skill:testIntent').require('testKeyword')
-        msg = Message('register_intent', intent.__dict__)
-        self.intent_service.handle_register_intent(msg)
-
-    def test_get_adapt_intent(self):
-        self.setup_simple_adapt_intent()
-        # Check that the intent is returned
-        msg = Message('intent.service.adapt.get',
-                      data={'utterance': 'test'})
-        self.intent_service.handle_get_adapt(msg)
-
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent']['intent_type'],
-                         'skill:testIntent')
-
-    def test_get_adapt_intent_no_match(self):
-        """Check that if the intent doesn't match at all None is returned."""
-        self.setup_simple_adapt_intent()
-        # Check that no intent is matched
-        msg = Message('intent.service.adapt.get',
-                      data={'utterance': 'five'})
-        self.intent_service.handle_get_adapt(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent'], None)
-
-    def test_get_intent(self):
-        """Check that the registered adapt intent is triggered."""
-        self.setup_simple_adapt_intent()
-        # Check that the intent is returned
-        msg = Message('intent.service.adapt.get',
-                      data={'utterance': 'test'})
-        self.intent_service.handle_get_intent(msg)
-
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent']['intent_type'],
-                         'skill:testIntent')
-
-    def test_get_intent_no_match(self):
-        """Check that if the intent doesn't match at all None is returned."""
-        self.setup_simple_adapt_intent()
-        # Check that no intent is matched
-        msg = Message('intent.service.intent.get',
-                      data={'utterance': 'five'})
-        self.intent_service.handle_get_intent(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent'], None)
-
-    def test_get_intent_manifest(self):
-        """Check that if the intent doesn't match at all None is returned."""
-        self.setup_simple_adapt_intent()
-        # Check that no intent is matched
-        msg = Message('intent.service.intent.get',
-                      data={'utterance': 'five'})
-        self.intent_service.handle_get_intent(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent'], None)
-
-    def test_get_adapt_intent_manifest(self):
-        """Make sure the manifest returns a list of Intent Parser objects."""
-        self.setup_simple_adapt_intent()
-        msg = Message('intent.service.adapt.manifest.get')
-        self.intent_service.handle_adapt_manifest(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intents'][0]['name'],
-                         'skill:testIntent')
-
-    def test_get_adapt_vocab_manifest(self):
-        self.setup_simple_adapt_intent()
-        msg = Message('intent.service.adapt.vocab.manifest.get')
-        self.intent_service.handle_vocab_manifest(msg)
-        reply = self.bus.emitted_msgs[-1]
-        value = reply.data['vocab'][0]['entity_value']
-        keyword = reply.data['vocab'][0]['entity_type']
-        self.assertEqual(keyword, 'testKeyword')
-        self.assertEqual(value, 'test')
-
-    def test_get_no_match_after_detach(self):
-        """Check that a removed intent doesn't match."""
-        self.setup_simple_adapt_intent()
-        # Check that no intent is matched
-        msg = Message('detach_intent',
-                      data={'intent_name': 'skill:testIntent'})
-        self.intent_service.handle_detach_intent(msg)
-        msg = Message('intent.service.adapt.get', data={'utterance': 'test'})
-        self.intent_service.handle_get_adapt(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent'], None)
-
-    def test_get_no_match_after_detach_skill(self):
-        """Check that a removed skill's intent doesn't match."""
-        self.setup_simple_adapt_intent()
-        # Check that no intent is matched
-        msg = Message('detach_intent',
-                      data={'skill_id': 'skill'})
-        self.intent_service.handle_detach_skill(msg)
-        msg = Message('intent.service.adapt.get', data={'utterance': 'test'})
-        self.intent_service.handle_get_adapt(msg)
-        reply = self.bus.emitted_msgs[-1]
-        self.assertEqual(reply.data['intent'], None)
-
-
-class TestAdaptIntent(TestCase):
-    """Test the AdaptIntent wrapper."""
-
-    def test_named_intent(self):
-        intent = AdaptIntent("CallEaglesIntent")
-        self.assertEqual(intent.name, "CallEaglesIntent")
-
-    def test_unnamed_intent(self):
-        intent = AdaptIntent()
-        self.assertEqual(intent.name, "")
