d83bfe33d78883503ee07ef89bcece1b3199cae1
miro <jarbasai@mailfence.com>
2025-03-13 03:24:02 +0000

    feat:lang_specific_skills_list

---
+++ test/install.sh
@@ -0,0 +1,13 @@
+
+uv pip install -r requirements/tests.txt
+uv pip install ./test/integrationtests/common_query/ovos_tskill_fakewiki
+uv pip install ./test/end2end/skill-ovos-hello-world
+uv pip install ./test/end2end/skill-ovos-fallback-unknown
+uv pip install ./test/end2end/skill-ovos-slow-fallback
+uv pip install ./test/end2end/skill-converse_test
+uv pip install ./test/end2end/skill-ovos-schedule
+uv pip install ./test/end2end/skill-new-stop
+uv pip install ./test/end2end/skill-old-stop
+uv pip install ./test/end2end/skill-fake-fm
+uv pip install ./test/end2end/skill-ovos-fakewiki
+uv pip install ./test/end2end/metadata-test-plugin
