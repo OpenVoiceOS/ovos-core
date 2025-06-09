73b7346d6a0d11a24f036b6be9100a9ba6ae86d1
miro <jarbasai@mailfence.com>
2025-06-09 21:14:02 +0100

    add model2vec test

--- test/end2end/test_helloworld.py
+++ test/end2end/test_helloworld.py
@@ -241,3 +241,98 @@ class TestPadatiousIntent(TestCase):
         )
 
         test.execute(timeout=10)
+
+
+class TestModel2VecIntent(TestCase):
+
+    def test_m2v_match(self):
+        skill_id = "ovos-skill-hello-world.openvoiceos"
+        session = Session("123")
+        session.pipeline = ["ovos-m2v-pipeline-high"]
+        message = Message("recognizer_loop:utterance",
+                          {"utterances": ["good morning"], "lang": "en-US"},
+                          {"session": session.serialize(), "source": "A", "destination": "B"})
+
+        test = End2EndTest(
+            skill_ids=[skill_id],
+            eof_msgs=["ovos.utterance.handled"],
+            flip_points=["recognizer_loop:utterance"],
+            source_message=message,
+            expected_messages=[
+                message,
+                Message(f"{skill_id}.activate",
+                        data={},
+                        context={"skill_id": skill_id}),
+                Message(f"{skill_id}:Greetings.intent",
+                        data={"utterance": "good morning", "lang": "en-US"},
+                        context={"skill_id": skill_id}),
+                Message("mycroft.skill.handler.start",
+                        data={"name": "HelloWorldSkill.handle_greetings"},
+                        context={"skill_id": skill_id}),
+                Message("speak",
+                        data={"lang": "en-US",
+                              "expect_response": False,
+                              "meta": {
+                                  "dialog": "hello",
+                                  "data": {},
+                                  "skill": skill_id
+                              }},
+                        context={"skill_id": skill_id}),
+                Message("mycroft.skill.handler.complete",
+                        data={"name": "HelloWorldSkill.handle_greetings"},
+                        context={"skill_id": skill_id}),
+                Message("ovos.utterance.handled",
+                        data={},
+                        context={"skill_id": skill_id}),
+            ]
+        )
+
+        test.execute(timeout=10)
+
+    def test_skill_blacklist(self):
+        skill_id = "ovos-skill-hello-world.openvoiceos"
+        session = Session("123")
+        session.pipeline = ["ovos-padatious-pipeline-plugin-high"]
+        session.blacklisted_skills = [skill_id]
+        message = Message("recognizer_loop:utterance",
+                          {"utterances": ["good morning"], "lang": "en-US"},
+                          {"session": session.serialize(), "source": "A", "destination": "B"})
+
+        test = End2EndTest(
+            skill_ids=[skill_id],
+            eof_msgs=["ovos.utterance.handled"],
+            flip_points=["recognizer_loop:utterance"],
+            source_message=message,
+            expected_messages=[
+                message,
+                Message("mycroft.audio.play_sound", {"uri": "snd/error.mp3"}),
+                Message("complete_intent_failure", {}),
+                Message("ovos.utterance.handled", {})
+            ]
+        )
+
+        test.execute(timeout=10)
+
+    def test_intent_blacklist(self):
+        skill_id = "ovos-skill-hello-world.openvoiceos"
+        session = Session("123")
+        session.pipeline = ["ovos-padatious-pipeline-plugin-high"]
+        session.blacklisted_intents = [f"{skill_id}:Greetings.intent"]
+        message = Message("recognizer_loop:utterance",
+                          {"utterances": ["good morning"], "lang": "en-US"},
+                          {"session": session.serialize(), "source": "A", "destination": "B"})
+
+        test = End2EndTest(
+            skill_ids=[skill_id],
+            eof_msgs=["ovos.utterance.handled"],
+            flip_points=["recognizer_loop:utterance"],
+            source_message=message,
+            expected_messages=[
+                message,
+                Message("mycroft.audio.play_sound", {"uri": "snd/error.mp3"}),
+                Message("complete_intent_failure", {}),
+                Message("ovos.utterance.handled", {})
+            ]
+        )
+
+        test.execute(timeout=10)
